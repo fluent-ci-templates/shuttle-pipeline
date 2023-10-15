6002a0e2c1e308ca19b390465165f1193b39f413
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-15 07:26:30 +0000

    fix queries

--- src/dagger/queries.ts
+++ src/dagger/queries.ts
@@ -2,6 +2,6 @@ import { gql } from "../../deps.ts";
 
 export const deploy = gql`
   query deploy($src: String!, $apiKey: String!) {
-    deply(src: $src, apiKey: $apiKey)
+    deploy(src: $src, apiKey: $apiKey)
   }
 `;
