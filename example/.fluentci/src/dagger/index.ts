4297246147873e2fc50de50361c8d681a77102c1
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-30 15:24:55 +0000

    remove GraphQL, use jsdocs for exported functions
    update deno.lock

--- example/.fluentci/src/dagger/index.ts
+++ example/.fluentci/src/dagger/index.ts
@@ -1,4 +1,4 @@
 import pipeline from "./pipeline.ts";
-import { deploy } from "./jobs.ts";
+import { deploy, jobDescriptions } from "./jobs.ts";
 
-export { pipeline, deploy };
+export { pipeline, deploy, jobDescriptions };
--- src/dagger/queries.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "../../deps.ts";
-
-export const deploy = gql`
-  query deploy($src: String!, $apiKey: String!) {
-    deploy(src: $src, apiKey: $apiKey)
-  }
-`;
