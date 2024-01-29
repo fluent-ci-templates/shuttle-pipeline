59381e87f76f3b29a3b1272d55a9e39c3df764da
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-29 07:08:25 +0000

    fix issue with dagger connect
    update deno.lock
    
    update deno.lock

--- example/.fluentci/src/dagger/jobs.ts
+++ example/.fluentci/src/dagger/jobs.ts
@@ -1,5 +1,4 @@
-import Client, { Directory, Secret } from "../../deps.ts";
-import { connect } from "../../sdk/connect.ts";
+import { Directory, Secret, dag } from "../../deps.ts";
 import { getDirectory, getApiKey } from "./lib.ts";
 
 export enum Job {
@@ -11,47 +10,43 @@ export const exclude = ["target", ".git", ".fluentci"];
 /**
  * @function
  * @description Deploy the application to Shuttle
- * @param src {string | Directory | undefined}
- * @param apiKey {string | Secret}
+ * @param {string | Directory | undefined} src
+ * @param {string | Secret} apiKey
  * @returns {string}
  */
 export async function deploy(
   src: string | Directory | undefined = ".",
   apiKey?: string | Secret
 ): Promise<string> {
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const secret = getApiKey(client, apiKey);
+  const context = await getDirectory(dag, src);
+  const secret = await getApiKey(dag, apiKey);
 
-    if (!secret) {
-      console.error("Missing SHUTTLE_API_KEY");
-      Deno.exit(1);
-    }
+  if (!secret) {
+    console.error("Missing SHUTTLE_API_KEY");
+    Deno.exit(1);
+  }
 
-    const ctr = client
-      .pipeline(Job.deploy)
-      .container()
-      .from("rust:1.72-bookworm")
-      .withExec(["apt", "update"])
-      .withExec(["apt", "install", "-y", "build-essential"])
-      .withMountedCache(
-        "/usr/local/cargo/registry",
-        client.cacheVolume("cargo-registry")
-      )
-      .withExec(["cargo", "install", "cargo-shuttle"])
-      .withSecretVariable("SHUTTLE_API_KEY", secret)
-      .withMountedCache("/app/target", client.cacheVolume("cargo-target"))
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["sh", "-c", "cargo shuttle login --api-key $SHUTTLE_API_KEY"])
-      .withExec(["cargo", "shuttle", "deploy"]);
+  const ctr = dag
+    .pipeline(Job.deploy)
+    .container()
+    .from("rust:1.72-bookworm")
+    .withExec(["apt", "update"])
+    .withExec(["apt", "install", "-y", "build-essential"])
+    .withMountedCache(
+      "/usr/local/cargo/registry",
+      dag.cacheVolume("cargo-registry")
+    )
+    .withExec(["cargo", "install", "cargo-shuttle"])
+    .withSecretVariable("SHUTTLE_API_KEY", secret)
+    .withMountedCache("/app/target", dag.cacheVolume("cargo-target"))
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", "cargo shuttle login --api-key $SHUTTLE_API_KEY"])
+    .withExec(["cargo", "shuttle", "deploy"]);
 
-    const result = await ctr.stdout();
+  const result = await ctr.stdout();
 
-    console.log(result);
-  });
-
-  return "done";
+  return result;
 }
 
 export type JobExec = (src?: string, apiKey?: string) => Promise<string>;
